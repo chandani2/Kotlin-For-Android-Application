4d0f8937ed7384c5534229e2113d1166eb32a368
chandani2 <chandani3103@gmail.com>
2022-05-19 12:14:43 +0530

    jst

--- DiceRoller/app/src/main/java/com/cris/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/cris/diceroller/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //btnRoll is val type veriable
         val btnRoll: Button = findViewById(R.id.btnRoll)
 
 
